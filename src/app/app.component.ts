b1344127545e8a8ee42e287b4761253057265eef
Lai Man Wai <SCMWLAI@slake.sssb.com>
2023-07-19 18:56:13 +0800

    19072023: Assignment 2 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udemy-assignment-2';
+  username:string = "";
+  isNotEmpty:boolean = false;
+
+  isBtnAvailable(){
+    if(this.username.length > 0){
+      this.isNotEmpty = true;
+    }else{
+      this.isNotEmpty = false;
+    }
+  }
+
+  resetUsername(){
+    this.username = "";
+    this.isNotEmpty = false;
+  }
+
+
+
 }
